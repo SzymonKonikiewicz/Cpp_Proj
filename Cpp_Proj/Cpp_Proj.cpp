662d748ca6c1cc4bbd7e4899e387fd2ae7380742
SzymonKonikiewicz <szymon.konikiewicz@gmail.com>
2025-12-18 16:55:32 +0100

    Update_18_12

--- Cpp_Proj/Cpp_Proj.cpp
+++ Cpp_Proj/Cpp_Proj.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 
 void mazeCreator(int maze[100][100], int size);
 void mazePrint(int maze[100][100], int size);
@@ -24,6 +25,17 @@ int main()
 
 static void mazeCreator(int maze[100][100], int size)
 {
+	bool flag{false};
+#if 0
+	My made up rules to the creation of the maze which will make it pretty :) (hopefully) :
+	1. Surrounded by walls //done
+	2. Has one entrance and one exit //entrance done
+	3. There is never a situation where a 4x4 block of walls is created at a border of a maze
+	4. Maze consists of ONLY a 1 tile wide corridors
+	5. Maze is possible to complete obviously
+
+#endif
+
 	for (int i = 0; i < 100; i++)
 	{
 		if (i < size) {
@@ -32,15 +44,35 @@ static void mazeCreator(int maze[100][100], int size)
 				//Borders of the maze
 				if (i == 0 || i == size-1 || j == 0 || j == size-1)
 				{
-					maze[i][j] = 3;
-				} else if (j < size) {
 					maze[i][j] = 1;
+				}
+				//Corners of the maze
+				else if ((i == 1 && j == 1) || (i == size - 2 && j == size - 2) || (i == size - 2 && j == 1) || (i == 1 && j == size - 2))
+				{
+					maze[i][j] = 0;
+				}
+				else if (j < size)
+				{
+					if (i == 1 || i == size - 2) {
+
+					} 
+					else if (rand()%3 == 0 && flag)
+					{
+						maze[i][j] = 0;
+						flag = false;
+					}
+					else 
+					{ 
+						maze[i][j] = 1;
+						flag = true;
+					}
 				}
 				else maze[i][j] = 0;
 			}
 		}
-		
 	}
+	//Entrence to the maze
+	maze[size - 1][size / 2] = 0;
 }
 
 static void mazePrint(int maze[100][100], int size)
@@ -51,11 +83,11 @@ static void mazePrint(int maze[100][100], int size)
 		{
 			if (maze[i][j] == 1)
 			{
-				std::cout << maze[i][j] << " ";
+				std::cout << " # ";
 			}
-			else if (maze[i][j] == 3)
+			else if (maze[i][j] == 0)
 			{
-				std::cout << "#";
+				std::cout << "   ";
 			}
 		}
 		std::cout << "\n";
